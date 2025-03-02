30c6aff8612c52d213e19ce9c5e78f129421acc3
MahediHasan03 <mahedihasanapon333@gmail.com>
2025-03-03 03:23:15 +0600

    allmost done

---
+++ script/button.js
@@ -0,0 +1,214 @@
+
+
+document.getElementById("button1").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn1 = document.getElementById("button1Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn1} at ${currentTime};
+        `
+
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+        
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+
+})
+//button2
+document.getElementById("button2").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn2 = document.getElementById("button2Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn2} at ${currentTime};
+        `
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+})
+// button3
+document.getElementById("button3").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn3 = document.getElementById("button3Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn3} at ${currentTime};
+        `
+
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+        
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+
+})
+// button4
+document.getElementById("button4").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn4 = document.getElementById("button4Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn4} at ${currentTime};
+        `
+
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+        
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+})
+// button5
+document.getElementById("button5").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn5 = document.getElementById("button5Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn5} at ${currentTime};
+        `
+
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+        
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+
+})
+// button6
+document.getElementById("button6").addEventListener("click", 
+    function(){
+        alert("Board Update Successfully");
+        alert("congratulation!!! You have complete all the current task.")
+        const container =document.getElementById("activity-box")
+        const p =document.createElement("p");
+        const btn6 = document.getElementById("button6Text").innerText;
+        let currentTime = new Date().toLocaleTimeString();
+        p.innerText=`
+        You have Complete The Task ${btn6} at ${currentTime};
+        `
+
+        // p tag style
+        p.style.display="flex";
+        p.style.alignItems="center";
+        p.style.backgroundColor= "white";
+        p.style.borderRadius="10px";
+        p.style.margin="10px";
+        p.style.paddingBottom="15px";
+        p.style.paddingLeft="15px";
+        container.appendChild(p)
+        
+
+        const taskCount = document.getElementById("taskAssigned").innerText;
+        const convertTaskCount = parseInt(taskCount);
+        const taskCount2= convertTaskCount-1;
+        document.getElementById("taskAssigned").innerText=taskCount2;
+
+        const navNumber = document.getElementById("navNumber").innerText;
+        const convertNavNumber = parseInt(navNumber);
+        const navNumber2= convertNavNumber+1;
+        document.getElementById("navNumber").innerText=navNumber2;
+
+        this.disabled = true;
+
+})
