6ad3ea9a7d373c48b6b2f86ef268f051db4dc76d
MahediHasan03 <mahedihasanapon333@gmail.com>
2025-03-03 16:36:59 +0600

    discover

--- script/discover.js
+++ script/discover.js
@@ -4,3 +4,9 @@ document.getElementById("discover").addEventListener("click",
     function(){
         window.location.href="./discover.html"
 })
+
+document.getElementById("discover-btn").addEventListener("click", 
+    function(){
+        window.location.href="./index.html"
+})
+
